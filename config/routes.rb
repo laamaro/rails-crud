b33e3fa54588904a26c3d49975d3aac9c6e0d753
Laura Amaro <lauraamaro1804@gmail.com>
2023-10-07 11:42:16 -0300

    finish lecture with crud complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants
+  # # Read ALL restaurants
+  # get 'restaurants', to: 'restaurants#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # # See form to input data of new restaurant
+  # get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+
+  # # Create a new restaurant with data from form
+  # post 'restaurants', to: 'restaurants#create'
+
+  # # Read details of ONE restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  # # See form to edit data of ONE restaurant
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+
+  # # Update restaurant with data from form
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # # Delete a restaurant
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
 end
